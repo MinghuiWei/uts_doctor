3086f42ae7e5036e12abd2f48e34f24df3e1f54d
Minghui <853982992@qq,com>
2018-09-24 13:25:36 +1000

    update db

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+
+$active_group = in_array(gethostname(), array("dev-leel")) ? 'default' : 'production';
 $query_builder = TRUE;
 
 $db['default'] = array(
