d7e443d2dccf3fcfdc8af8ba2caa70eb3685f0d7
MinghuiWei <853982992@qq,com>
2018-10-02 21:25:00 +1000

    add more unit test

--- application/tests/controllers/Home_test.php
+++ application/tests/controllers/Home_test.php
@@ -16,6 +16,48 @@ class Welcome_test extends TestCase
 		$this->assertContains('<title>Hospital Reservation System</title>', $output);
 	}
 
+	public function test_login()
+	{
+		$output = $this->request('GET', 'home/login');
+		$this->assertContains('Login', $output);
+	}
+
+	public function test_signup()
+	{
+		$output = $this->request('GET', 'home/signup');
+		$this->assertContains('Sign up', $output);
+	}
+
+	public function test_doctor()
+	{
+		$output = $this->request('GET', 'doctor/index');
+		$this->assertEquals(NULL, $output);
+	}
+
+	public function test_sec()
+	{
+		$output = $this->request('GET', 'secretary/index');
+		$this->assertEquals(NULL, $output);
+	}
+
+	public function test_patient()
+	{
+		$output = $this->request('GET', 'patient/index');
+		$this->assertEquals(NULL, $output);
+	}
+
+	public function test_admin()
+	{
+		$output = $this->request('GET', 'admin/index');
+		$this->assertEquals(NULL, $output);
+	}
+
+	public function test_doctor_appointments()
+	{
+		$output = $this->request('GET', 'doctor/appointments');
+		$this->assertEquals(NULL, $output);
+	}
+
 	public function test_method_404()
 	{
 		$this->request('GET', 'home/method_not_exist');
---
+++ test.sh
@@ -0,0 +1,1 @@
+cd application/tests/; ../../vendor/bin/phpunit
